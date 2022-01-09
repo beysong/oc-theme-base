db6bea66cecbd3d4891472417ec7bbd1f7ef6437
wangsong <415613212@qq.com>
2022-01-09 13:55:13 +0800

    backup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("daisyui"),
   ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
   daisyui: {
     // styled: false,
     themes: [
