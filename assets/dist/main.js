06f1d1776de1f8346d8c294c570a03be44b84b01
wangsong <415613212@qq.com>
2022-06-06 09:24:31 +0800

    backup

--- assets/dist/main.js
+++ assets/dist/main.js
@@ -70,8 +70,15 @@
   \*********************************/
 /***/ (() => {
 
-(function ($) {
-  var swiper = new Swiper(".swiper", {
+(function (Swiper) {
+  var swiper = new Swiper(".commonSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false
+    }
+  });
+  var mainSlider = new Swiper(".mainSwiper", {
     loop: true,
     autoplay: {
       delay: 5500,
